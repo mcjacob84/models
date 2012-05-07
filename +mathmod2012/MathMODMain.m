f664eb0239f9b166e98ae3f9d6b04d69ab420014
Daniel Wirtz <daniel.wirtz@mathematik.uni-stuttgart.de>
2012-05-07 13:26:03 -0500

    using PlotManager for MathMOD experiments now

--- +mathmod2012/MathMODMain.m
+++ +mathmod2012/MathMODMain.m
@@ -1,14 +1,13 @@
 function [m, r, d] = MathMODMain(dim)
-    % Current version works with KerMor 0.5
+    % Current version works with KerMor 0.6
 
 %     clear classes;
     if nargin == 0
         dim = 1000;
     end
     
-    loc = '/home/dwirtz/aghhome/Material/images';
-    figsize = [800 500];
-    
+    %loc = '/home/dwirtz/aghhome/Material/images';
+    loc = 'C:/Users/CreaByte/Documents/Uni/img';
     
     %in = 1;
     
@@ -16,41 +15,34 @@ function [m, r, d] = MathMODMain(dim)
     m.offlineGenerations;
     r = m.buildReducedModel;
     
-%     d = tools.EstimatorAnalyzer;
-%     d.EstimatorIterations = [1 2 5];
-%     d.EstimatorVersions = [1 1 0 0 1 0 0 1 1];
-%     d.SingleFigures = true;
-%     d.setModel(r);
-%     d.start([.5; 5; -.2],[]);
+    d = tools.EstimatorAnalyzer;
+    d.EstimatorIterations = [1 2 5];
+    d.EstimatorVersions = [1 1 0 0 1 0 0 1 1];
+    d.setModel(r);
+    pm = tools.PlotManager(false,2,2);
+    pm.FilePrefix = 'mmod_noinput_';
+    %d.start([.5; 5; -.2],[],pm);
+    d.start([.5; 1; -.2],[],pm);
+    pm.done;
+%     pm.savePlots(loc,{'fig','pdf','jpg'});
+    
+%     pm = tools.PlotManager(false,2,2);
+%     pm.FilePrefix = 'mmod12';
+%     pm.SingleSize = [800 500];
+%     % Pure inner dynamics without inputs and initial value -.2
+%     tools.ParamSweep(r, [0; 10; -.2], [], 2, 0:1:10, ...
+%         pm.nextPlot('mmod_noinput_ps_exppar'));
 %     
-%     names = {'abserr','relerr','ctimes'};
-%     prefix = 'mmod_noinput_';
-%     for i = 1:3
-%         saveFig(i,fullfile(loc,[prefix names{i}]));
-%     end
-    
-    % Pure inner dynamics without inputs and initial value -.2
-    fig = tools.ParamSweep(r, [0; 10; -.2], [], 2, 0:1:10);
-    saveFig(fig, fullfile(loc,'mmod_noinput_ps_exppar'),'png');
-    
-    % Pure inner dynamics without inputs and initial value -.2
-    fig = tools.ParamSweep(r, [0; 3; -.2], 1, 1, 0:.1:1);
-    saveFig(fig, fullfile(loc,'mmod_ps_inputpar'),'png');
-    
-    fig = tools.ParamSweep(r, [0; 4; -.2], 2, 1, 0:.1:1);
-    saveFig(fig, fullfile(loc,'mmod_ps_inputpar2'),'png');
-    
-    fig = tools.ParamSweep2D(r, [0; 3; -.2], 1, [1 2], -.5:.2:1.5,-8:1.5:8);
-    saveFig(fig, fullfile(loc,'mmod_ps2d_in_exp'),'png');
-
-    function saveFig(fig, fn, fmt)
-        if nargin < 3
-            fmt = 'pdf';
-        end
-        general.Utils.saveFigure(fig, fn,'fig');
-        p = get(fig,'Position');
-        set(fig,'Position',[p(1:2) figsize]);
-        general.Utils.saveFigure(fig, fn,fmt);
-        close(fig);
-    end
+%     % Pure inner dynamics without inputs and initial value -.2
+%     tools.ParamSweep(r, [0; 3; -.2], 1, 1, 0:.1:1, ...
+%         pm.nextPlot('mmod_ps_inputpar'));
+%     
+%     tools.ParamSweep(r, [0; 4; -.2], 2, 1, 0:.1:1, ...
+%         pm.nextPlot('mmod_ps_inputpar2'));
+%     
+%     tools.ParamSweep2D(r, [0; 3; -.2], 1, [1 2], -.5:.2:1.5,-8:1.5:8, ...
+%         pm.nextPlot('mmod_ps2d_in_exp'));
+%     pm.done;
+%     
+%     pm.savePlots(loc,{'fig','pdf','jpg'});
 end
