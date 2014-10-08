f9c9d01f9c91cbb27f257b6548b7679c03597c24
Daniel Wirtz <daniel.wirtz@simtech.uni-stuttgart.de>
2014-10-08 13:03:46 +0200

    moved createJac to Sarcomere added frequency learning tests to motoneuron model

---
+++ +motoneuron/+experiments/FrequencyLearning.m
@@ -0,0 +1,59 @@
+clear classes;
+basedir = fileparts(mfilename('fullpath'));
+
+file = fullfile(basedir, 'paramdomaindetection_withnoise.mat');
+load(file);
+
+%% assemble training data
+m = min(ps,[],2);
+M = max(ps,[],2);
+n = 200;
+ft = linspace(m(1),M(1),n);
+mc = linspace(m(2),M(2),n);
+[FT,MC] = meshgrid(ft,mc);
+HZ = griddata(ps(1,:),ps(2,:),Hz,FT,MC,'nearest');
+
+%% smooth out data
+padsize = 6;
+stencil = [0 0 1 0 0; 0 1 1 1 0; 1 1 1 1 1; 0 1 1 1 0; 0 0 1 0 0];
+% stencil = [1 1 1; 1 1 1; 1 1 1];
+% stencil = [0 1 0; 1 1 1; 0 1 0];
+stencil = stencil/sum(stencil(:));
+HZ = padarray(HZ,[padsize padsize],'replicate');
+runs = 10;
+for k = 1:runs
+    HZ = conv2(HZ,stencil,'same');
+end
+HZ = HZ(padsize+1:end-padsize,padsize+1:end-padsize);
+mesh(FT,MC,HZ);
+axis tight;
+
+%% Init
+s = load(fullfile(basedir,'../','upperlimitpoly.mat'));
+maxv = polyval(s.upperlimit_poly,FT);
+valid = MC <= maxv;
+atd = data.ApproxTrainData([FT(valid) MC(valid)]',[],[]);
+atd.fxi = HZ(valid)';
+
+%% init algorithm
+alg = approx.algorithms.VKOGA;
+ec = kernels.config.ExpansionConfig;
+comb = Utils.createCombinations([1 2 3 4],[1 2 3]);
+sc = kernels.config.WendlandConfig('G',comb(1,:),'S',comb(2,:));
+ec.StateConfig = sc;
+alg.ExpConfig = ec;
+alg.MaxExpansionSize = 1000;
+
+%% compute
+kexp = alg.computeApproximation(atd);
+
+%% plot
+afx = kexp.evaluate([FT(:) MC(:)]');
+FX = reshape(afx,200,[]);
+FX(~valid) = Inf;
+mesh(FT,MC,FX);
+
+file = fullfile(basedir, 'FreqencyLearning.mat');
+save(file, 'alg', 'atd', 'HZ', 'FT', 'MC', 'valid');
+file = fullfile(basedir, 'FrequencyKexp.mat');
+save(file, 'kexp');
